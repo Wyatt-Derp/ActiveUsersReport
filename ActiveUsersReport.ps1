b5fdf9263623d5a40f520971799c260e019523de
dtang <dtang>
2018-07-26 15:47:13 -0700

    Initial Commit

---
+++ ActiveUsersReport.ps1
@@ -0,0 +1,3 @@
+$ActiveUsers = Get-ADUser –filter * -Properties passwordLastSet,whencreated,lastlogondate,Enabled,PasswordNeverExpires | Where {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.PasswordNeverExpires -eq $False -and $_.Enabled -eq $true} | Get-ADUser -Properties Name,lastlogondate | Select Name,lastlogondate
+
+$Outofcompliance = Get-ADUser –filter * -Properties passwordLastSet,whencreated,lastlogondate,Enabled,PasswordNeverExpires | Where {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.PasswordNeverExpires -eq $true -and $_.Enabled -eq $True} | Get-ADUser -Properties Name,lastlogondate | Select Name,lastlogondate
