ba10797735e3a333670700057a99d032b0ad8bee
dtang <dtang>
2018-07-27 16:46:32 -0700

    Imported ad module modified script to find 2 lists: all users users that don't have certain keywords in the name

--- ActiveUsersReport.ps1
+++ ActiveUsersReport.ps1
@@ -1,3 +1,9 @@
-$ActiveUsers = Get-ADUser –filter * -Properties passwordLastSet,whencreated,lastlogondate,Enabled,PasswordNeverExpires | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.PasswordNeverExpires -eq $False -and $_.Enabled -eq $true} | Get-ADUser -Properties Name,lastlogondate | Select-Object Name,lastlogondate
+Import-Module ActiveDirectory
+#List of keywords in Name of each user to exclude
+$blacklist = $blacklist = "svc|Mailbox|srvc"
 
-$Outofcompliance = Get-ADUser –filter * -Properties passwordLastSet,whencreated,lastlogondate,Enabled,PasswordNeverExpires | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.PasswordNeverExpires -eq $true -and $_.Enabled -eq $True} | Get-ADUser -Properties Name,lastlogondate | Select-Object Name,lastlogondate
+#Find all users that are enabled and logged in within the past 90 days.
+$AllUsers = Get-ADUser –filter * -Properties passwordLastSet,whencreated,lastlogondate,Enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true} | Get-ADUser -Properties Name,lastlogondate | Select-Object Name,lastlogondate
+
+#Find all users that are enabled and logged in within the past 90 days where names don't contain strings in the $blacklist
+$ActiveUsers = Get-ADUser –filter * -Properties passwordLastSet,whencreated,lastlogondate,Enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true -and $_.Name -inotmatch $blacklist} | Get-ADUser -Properties Name,lastlogondate | Select-Object Name,lastlogondate
