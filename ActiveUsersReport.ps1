932d0284e2295f36b88141f0a8bf0c7df1c28840
dtang <dtang>
2018-07-30 15:52:02 -0700

    added more blacklist terms added total to end of each list of users

--- ActiveUsersReport.ps1
+++ ActiveUsersReport.ps1
@@ -1,12 +1,44 @@
 Import-Module ActiveDirectory
 #List of keywords in Name of each user to exclude
-$blacklist = "svc|Mailbox|srvc|admin|billing|support|lab|ctac|scan|sharepoint|windows|window|n-able|nable|cluster|test|local|ACE|AAD|ncentral|MSOL|Open DNS|opendns|OneLogin|Airwatch|Temp|service"
+$blacklist = "svc|Mailbox|srvc|admin|billing|support|lab|ctac|scan|sharepoint|windows|window|n-able|nable|cluster|test|local|ACE|AAD|ncentral|MSOL|Open DNS|opendns|OneLogin|Airwatch|Temp|service|mastercontrol|copier|scan|fax|room"
 
 #Find all users that are enabled and logged in within the past 90 days.
-$AllUsers = Get-ADUser -filter * -Properties name,lastlogondate,enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true} | Select-Object Name,lastlogondate | Export-Csv -NoTypeInformation -Path c:\temp\allusers.csv
+$AllUsers = Get-ADUser -filter * -Properties name,lastlogondate,enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true} | Select-Object Name,lastlogondate
+
+$AllUsersCount = $AllUsers | measure
+$AllUsersTotal = New-Object -TypeName psobject -Property @{
+    Name = 'User Total:'
+    lastlogondate = $AllUsersCount.Count
+}
+$AllUsers += $AllUsersTotal
 
 #Find all users that are enabled and logged in within the past 90 days where names don't contain strings in the $blacklist
-$ActiveUsers = Get-ADUser -filter * -Properties name,lastlogondate,enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true -and $_.Name -inotmatch $blacklist} | Select-Object Name,lastlogondate | Export-Csv -NoTypeInformation -Path c:\temp\activeusers.csv
+$ActiveUsers = Get-ADUser -filter * -Properties name,lastlogondate,enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true -and $_.Name -inotmatch $blacklist} | Select-Object Name,lastlogondate
 
+$ActiveUsersCount = $ActiveUsers | measure
+$ActiveUsersTotal = New-Object -TypeName psobject -Property @{
+    Name = 'User Total:'
+    lastlogondate = $ActiveUsersCount.Count
+}
+$ActiveUsers += $ActiveUsersTotal
 
+#Create objects to store at the end of the $allusers variable to demarcate beginning of next list
+$breakDelimiter = New-Object -TypeName psobject -Property @{
+    Name = '=========='
+    lastlogondate = '=========='
+}
+$activeUsersdelimiter = New-Object -TypeName psobject -Property @{
+    Name = 'Active Users'
+    lastlogondate = ''
+}
 
+#Add objects to $allusers array for a break
+$reportBreak += $breakDelimiter
+$reportBreak += $activeUsersdelimiter
+$reportBreak += $breakDelimiter
+
+#Combine both user reports, create filename and export to csv
+$userReport = $AllUsers + $reportBreak + $ActiveUsers
+$filepath = "c:\temp\UserReport_" + (get-date).ToString("MM-dd-yyyy") + ".csv"
+$userReport
+#$userReport | Export-Csv -NoTypeInformation -Path $filepath
