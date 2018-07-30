064a0dccc7c98318f0596d140d62e9297b60a0b6
dtang <dtang>
2018-07-30 11:45:32 -0700

    cleaned up information pulled from get-aduser

--- ActiveUsersReport.ps1
+++ ActiveUsersReport.ps1
@@ -1,9 +1,12 @@
 Import-Module ActiveDirectory
 #List of keywords in Name of each user to exclude
-$blacklist "svc|Mailbox|srvc|admin|billing|support|lab|ctac|scan|sharepoint|windows|window|n-able|nable|cluster"
+$blacklist = "svc|Mailbox|srvc|admin|billing|support|lab|ctac|scan|sharepoint|windows|window|n-able|nable|cluster"
 
 #Find all users that are enabled and logged in within the past 90 days.
-$AllUsers = Get-ADUser –filter * -Properties passwordLastSet,whencreated,lastlogondate,Enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true} | Get-ADUser -Properties Name,lastlogondate | Select-Object Name,lastlogondate | Export-Csv -NoTypeInformation -Path c:\temp\allusers.csv
+$AllUsers = Get-ADUser -filter * -Properties name,lastlogondate,enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true} | Select-Object Name,lastlogondate | Export-Csv -NoTypeInformation -Path c:\temp\allusers.csv
 
 #Find all users that are enabled and logged in within the past 90 days where names don't contain strings in the $blacklist
-$ActiveUsers = Get-ADUser –filter * -Properties passwordLastSet,whencreated,lastlogondate,Enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true -and $_.Name -inotmatch $blacklist} | Get-ADUser -Properties Name,lastlogondate | Select-Object Name,lastlogondate | Export-Csv -NoTypeInformation -Path c:\temp\activeusers.csv
+$ActiveUsers = Get-ADUser -filter * -Properties name,lastlogondate,enabled | Where-Object {$_.lastlogondate -gt (get-date).adddays(-90) -and $_.Enabled -eq $true -and $_.Name -inotmatch $blacklist} | Select-Object Name,lastlogondate | Export-Csv -NoTypeInformation -Path c:\temp\activeusers.csv
+
+
+
